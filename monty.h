6d344575412af8d570e4231a65d08f35dc276e3d
EucTech <euccreative@gmail.com>
2023-06-22 17:29:01 +0000

    fixed error

--- monty.h
+++ monty.h
@@ -60,5 +60,5 @@ void _sub(stack_t **stack, unsigned int line_number);
 void _mul(stack_t **stack, unsigned int line_number);
 void _div(stack_t **stack, unsigned int line_number);
 void _mod(stack_t **stack, unsigned int line_number);
-void _pstr(stack_t **stack, __attribute__((unused))unsigned int line_number);
+void _pstr(stack_t **stack, unsigned int line_number);
 #endif
